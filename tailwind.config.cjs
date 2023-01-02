8da0395f53b2c3428b31c056250957f2a742f929
Jack Doherty <jack.doherty@ericsson.com>
2023-01-02 03:50:09 +0000

    add footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+    },
+      extend: {},
   },
   plugins: [],
-};
+}
