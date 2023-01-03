798aa43b1000bdabd7747166d27960107f88aace
Jack Doherty <jack.doherty@ericsson.com>
2023-01-03 00:52:07 +0000

    fix layouts to play nice with sessions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,33 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
 
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
-
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+type AppPropsWithLayout<P> = AppProps<P> & {
+  Component: NextPageWithLayout<P>;
+};
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
-}) => {
+  pageProps,
+}: AppPropsWithLayout<{ session: Session | null }>) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-  const getLayout = Component.getLayout ?? ((page: NextPage) => page)
-
-  return getLayout(
-    <SessionProvider session={session}>
-        <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      {getLayout(<Component {...pageProps} />)}
     </SessionProvider>
   );
 };
 
 export default api.withTRPC(MyApp);
-
