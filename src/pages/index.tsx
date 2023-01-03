bb9ad7d10f0e2e4ace4449cd63879d5adc500ec6
Jack Doherty <jack.doherty@ericsson.com>
2023-01-03 23:00:32 +0000

    add loading spinner to apply button once clicked

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -166,7 +166,34 @@ const Home: NextPageWithLayout = () => {
                     type="submit"
                     className="mt-2 inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
                   >
-                    Apply
+                    {newApplicant.isLoading ? (
+                      <div className="flex items-center justify-center">
+                        <svg
+                          
+                          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8v1a7 7 0 00-7 7h1z"
+                          ></path>
+                          </svg>
+                          <span className="text-white">Loading...</span>
+                          </div>
+                          ) : (
+                            "Apply"
+                          )}
                   </button>
                 </div>
               </div>
