52d06d8bd1c41bdcaa9e833702279b995dcae5bd
Jack Doherty <jack.doherty@ericsson.com>
2023-01-07 16:28:43 +0000

    centre mayhem text on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -101,7 +101,7 @@ const Footer = () => {
         </Modal>
       </div>
       <div className="w-full text-center">
-        <p className="mr-14">© 2023 MAYH3M</p>
+        <p className="mr-10">© 2023 MAYH3M</p>
       </div>
 
       <div className="flex">
