c53eb2a4937c6ef857665751823173e316bbc26b
Jack Doherty <jack.doherty@ericsson.com>
2023-01-07 15:42:26 +0000

    remove boilerplate

--- src/pages/drop/[dropId].tsx
+++ src/pages/drop/[dropId].tsx
@@ -5,22 +5,18 @@ const Drop = () => {
   const router = useRouter();
 
   const dropId = router.query.dropId as string;
-  const drops = api.drop.getDrop.useQuery({
+  const drop = api.drop.getDrop.useQuery({
     id: dropId,
   });
-
-  if (drops.isLoading) return <div>Loading...</div>;
-  if (drops.isError) return <div>Error: {drops.error.message}</div>;
-
   return (
+    
+    {drop.data ? 
+
     <div>
-      <h1>{data.drop.name}</h1>
-      <p>{data.drop.description}</p>
-      <p>{data.drop.price}</p>
-      <p>{data.drop.quantity}</p>
-      <p>{data.drop.releaseDate}</p>
-      <p>{data.drop.releaseTime}</p>
+      <h1>{drop.data.name}</h1>
+      <p>{drop.data.description}</p>
     </div>
+    : <div>Loading...</div>}
   );
 };
 
