709d53086c3a4a793a6f7cc49dc8fc74c87662d9
Jack Doherty <jack.doherty@ericsson.com>
2023-01-09 00:52:51 +0000

    add chache time 0 to has applied query to not show form after user has already applied: loading is false is cached so form is shown when user has already applied okbye

--- src/components/DropApplicationForm.tsx
+++ src/components/DropApplicationForm.tsx
@@ -18,10 +18,10 @@ const DropApplicationForm: React.FC<DropApplicationFormProps> = ({
   const createNewApplication = api.application.createApplication.useMutation();
   const hasApplied = api.user.hasUSerApplied.useQuery(
     { dropId: dropId },
-    { enabled: session ? true : false }
+    { enabled: session ? true : false, cacheTime: 0 }
   );
 
-  console.log(createNewApplication.isSuccess || hasApplied.data || false);
+  console.log(!hasApplied.data, hasApplied.isLoading);
 
   const {
     register,
@@ -41,92 +41,94 @@ const DropApplicationForm: React.FC<DropApplicationFormProps> = ({
     return <div>Loading...</div>;
   }
 
+  if (hasApplied.data || createNewApplication.isSuccess) {
+    return (
+      <div className="text-center">
+        <p className="text-xl font-bold">YOU APPLIED FOR THIS DROP</p>
+        <p className="text-xs italic">
+          Keep an eye on your email for updates from us about your application
+        </p>
+        <p className="mt-4 text-xl font-bold">GOOD LUCK</p>
+        <p className="text-xs italic">FROM THE MAYH3M TEAM</p>
+        <p className=" mb-2 mt-8 text-xs italic">
+          Check out our other upcoming drops
+        </p>
+        <Link className=" bg-black p-2 text-sm font-bold text-white" href="/">
+          UPCOMING DROPS
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <>
-      {hasApplied.data || createNewApplication.isSuccess ? (
-        <div className="text-center">
-          <p className="text-xl font-bold">YOU APPLIED FOR THIS DROP</p>
-          <p className="text-xs italic">
-            Keep an eye on your email for updates from us about your application
-          </p>
-          <p className="mt-4 text-xl font-bold">GOOD LUCK</p>
-          <p className="text-xs italic">FROM THE MAYH3M TEAM</p>
-          <p className=" mb-2 mt-8 text-xs italic">
-            Check out our other upcoming drops
-          </p>
-          <Link className=" bg-black p-2 text-sm font-bold text-white" href="/">
-            UPCOMING DROPS
-          </Link>
-        </div>
-      ) : (
-        <div className="w-3/4 text-center sm:w-1/2">
-          <form onSubmit={onSubmit}>
-            <div className="">
-              <p className="text-lg font-bold">JOIN THE DROP</p>
-              <p className="my-2 text-xs italic">
-                Apply below for a chance to be involved in the drop.
-              </p>
-              <p className="mb-4 text-xs italic">
-                100 units will be released, only those who apply and are
-                accepted will get to the chance to purchase.
-              </p>
-              <label className="text-xs font-bold" htmlFor="why">
-                WHY DO YOU WANT TO JOIN THIS DROP?
-              </label>
-            </div>
-            <div>
-              <textarea
-                required
-                {...register("why", {
-                  required: true,
-                  maxLength: 1000,
-                  minLength: 10,
-                })}
-                className="mt-2 h-40 w-full border p-2 focus:outline-none"
-                placeholder="why..."
-              />
+      <div className="w-3/4 text-center sm:w-1/2">
+        <form onSubmit={onSubmit}>
+          <div className="">
+            <p className="text-lg font-bold">JOIN THE DROP</p>
+            <p className="my-2 text-xs italic">
+              Apply below for a chance to be involved in the drop.
+            </p>
+            <p className="mb-4 text-xs italic">
+              100 units will be released, only those who apply and are accepted
+              will get to the chance to purchase.
+            </p>
+            <label className="text-xs font-bold" htmlFor="why">
+              WHY DO YOU WANT TO JOIN THIS DROP?
+            </label>
+          </div>
+          <div>
+            <textarea
+              required
+              {...register("why", {
+                required: true,
+                maxLength: 1000,
+                minLength: 10,
+              })}
+              className="mt-2 h-40 w-full border p-2 focus:outline-none"
+              placeholder="why..."
+            />
 
-              {createNewApplication.isLoading ? (
-                // show loading spinner in centre of button
-                <div className="w-full bg-black p-2 text-center text-xs text-white ">
-                  <svg
-                    className="mx-auto h-4 w-4 animate-spin text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8v1a7 7 0 00-7 7h1z"
-                    ></path>
-                  </svg>
-                </div>
-              ) : (
-                <button
-                  className="w-full bg-black p-2 text-center text-xs text-white hover:opacity-50"
-                  type="submit"
+            {createNewApplication.isLoading ? (
+              // show loading spinner in centre of button
+              <div className="w-full bg-black p-2 text-center text-xs text-white ">
+                <svg
+                  className="mx-auto h-4 w-4 animate-spin text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
                 >
-                  SUBMIT
-                </button>
-              )}
-              {errors.why && (
-                <p className="mt-2 text-xs  italic text-red-500">
-                  Please enter a valid reason (10-1000 characters).
-                </p>
-              )}
-            </div>
-          </form>
-        </div>
-      )}
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v1a7 7 0 00-7 7h1z"
+                  ></path>
+                </svg>
+              </div>
+            ) : (
+              <button
+                className="w-full bg-black p-2 text-center text-xs text-white hover:opacity-50"
+                type="submit"
+              >
+                SUBMIT
+              </button>
+            )}
+            {errors.why && (
+              <p className="mt-2 text-xs  italic text-red-500">
+                Please enter a valid reason (10-1000 characters).
+              </p>
+            )}
+          </div>
+        </form>
+      </div>
     </>
   );
 };
