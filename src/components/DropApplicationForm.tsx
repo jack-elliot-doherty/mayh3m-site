0f444956fe84a45326ea9a3602338b019cb14831
Jack Doherty <jack.doherty@ericsson.com>
2023-01-09 00:20:23 +0000

    centre apply loading spinner

--- src/components/DropApplicationForm.tsx
+++ src/components/DropApplicationForm.tsx
@@ -88,26 +88,28 @@ const DropApplicationForm: React.FC<DropApplicationFormProps> = ({
 
               {createNewApplication.isLoading ? (
                 // show loading spinner in centre of button
-                <svg
-                  className=" h-5 w-5 animate-spin text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v1a7 7 0 00-7 7h1z"
-                  ></path>
-                </svg>
+                <div className="w-full bg-black p-2 text-center text-xs text-white ">
+                  <svg
+                    className="mx-auto h-4 w-4 animate-spin text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v1a7 7 0 00-7 7h1z"
+                    ></path>
+                  </svg>
+                </div>
               ) : (
                 <button
                   className="w-full bg-black p-2 text-center text-xs text-white hover:opacity-50"
